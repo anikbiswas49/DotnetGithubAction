cebf46a9c5885ff9d3d4e55cb342728b20486b03
Anik Biswas <anik.biswas@eps.com.bd>
2024-01-07 22:55:34 +0600

    init commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DotentGithubAction.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
